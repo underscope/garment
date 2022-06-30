0baf9a3c6b3bc757ddd2a0f2db2685557d3423ca
underscope <underscope@gmail.com>
2022-06-30 10:31:04 +0200

    Add getter for resolving fileKey

--- src/garment/entities/Repository.ts
+++ src/garment/entities/Repository.ts
@@ -7,6 +7,7 @@ import { Activity } from './Activity'
 
 export class Repository {
   static api: any
+
   envPath: string
   isLoaded = false
 
@@ -23,12 +24,16 @@ export class Repository {
   @Type(() => Date)
   publishedAt: Date
 
-  get size(): string {
-    return bytes(sizeof(this))
+  get fileKey() {
+    return this.id.toString()
   }
 
   get path(): string {
-    return Repository.api.getRepositoryPath(this.id.toString(), this.envPath)
+    return Repository.api.getRepositoryPath(this.fileKey, this.envPath)
+  }
+
+  get size(): string {
+    return bytes(sizeof(this))
   }
 
   async load() {
@@ -42,11 +47,11 @@ export class Repository {
   }
 
   snapshot(version = new Date().getTime().toString()) {
-    const directory = `${this.id}/${version}`
+    const directory = `${this.fileKey}/${version}`
     return Repository.api.cloneToEnv(this.path, GarmentEnv.Snapshot, directory)
   }
 
   getContainer(id: string) {
-    return Repository.api.getContainer(id, this.id.toString())
+    return Repository.api.getContainer(id, this.fileKey)
   }
 }
