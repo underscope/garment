8a1afc4d10330c08b6eb0dc0b94ddec2556cb1f7
underscope <underscope@gmail.com>
2022-07-01 10:48:34 +0200

    Add ability to sign internal assets

--- src/garment/entities/content-element/ContentElement.ts
+++ src/garment/entities/content-element/ContentElement.ts
@@ -1,6 +1,11 @@
 import bytes from 'bytes'
+import set from 'lodash/set'
 import sizeof from 'object-sizeof'
 import { Type } from 'class-transformer'
+
+// seconds, 12 hrs
+const DEFAULT_ACCESS_TOKEN_INTERVAL = 12 * 60 * 60
+const INTERNAL_STORAGE_PROTOCOL = 'storage://'
 
 export class ContentElement {
   static api: any
@@ -11,9 +16,11 @@ export class ContentElement {
   contentSignature: string
   type: string
   position: number
-  data: any
-  meta: any
   refs: any
+  meta: { [key: string]: any }
+  data: {
+    assets: { [key: string]: string }
+  }
 
   @Type(() => Date)
   createdAt: Date
@@ -23,5 +30,39 @@ export class ContentElement {
 
   get size(): string {
     return bytes(sizeof(this))
+  }
+
+  makePublic(interval = DEFAULT_ACCESS_TOKEN_INTERVAL) {
+    return this.processAssets(interval)
+  }
+
+  /**
+   * Signs all asset urls within the data.assets key-value pair definition;
+   * adds token param which enables access for a limited time.
+   * Key denotes location within the data property where the resolved asset url
+   * should be set (e.g. assets['deep.url'] = 'storage://test.png' should
+   * result in signed 'storage://test.png' url set on data['deep.url'] path).
+   */
+  private async processAssets(interval: number) {
+    if (!this.data?.assets) return
+    const assets = Object.entries(this.data.assets)
+    await Promise.all(assets.map(async ([keyWithinData, url = '']) => {
+      url = this.isStorageAsset(url)
+        ? await this.getSignedAssetUrl(url, interval)
+        : url
+      set(this.data, keyWithinData, url)
+    }))
+  }
+
+  private getSignedAssetUrl(url: string, interval: number) {
+    return ContentElement.api.getSignedUrl(this.getAssetPath(url), interval)
+  }
+
+  private isStorageAsset(url: string) {
+    return url.startsWith(INTERNAL_STORAGE_PROTOCOL)
+  }
+
+  private getAssetPath(url: string) {
+    return url.substring(INTERNAL_STORAGE_PROTOCOL.length)
   }
 }
