01e698cf1a515ceafc5732f47f6824b5aeee2697
underscope <underscope@gmail.com>
2022-07-01 11:08:18 +0200

    Expose new entities from entity root

--- src/garment/entities/index.ts
+++ src/garment/entities/index.ts
@@ -1,9 +1,13 @@
 import { Activity } from './activity'
 import { CatalogItem } from './CatalogItem'
+import { ContentContainer } from './content-container'
+import { ContentElement } from './content-element'
 import { Repository } from './repository'
 
 export {
   Activity,
   CatalogItem,
+  ContentContainer,
+  ContentElement,
   Repository,
 }
