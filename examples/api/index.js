e47a0ab42a615e85fa38e5a65ae4a9d38a29a1b2
underscope <underscope@gmail.com>
2022-08-02 19:00:56 +0200

    Expose sample api
    - list items
    - get repo

--- examples/api/index.js
+++ examples/api/index.js
@@ -1,13 +1,24 @@
 const express = require('express')
-const app = express()
+const { Garment } = require('../../dist/index.cjs')
+const config = require('./config')
 
 const { log } = console
-const port = 3030
+const app = express()
+const garment = new Garment(config.storage)
 
-app.get('/', (_, res) => {
-  res.send('Hello World, from express')
+app.get('/', async (_, res) => {
+  const catalog = await garment.source().list()
+  res.json(catalog)
 })
 
-app.listen(port, () => {
-  log(`Garment example API listening on port ${port}`)
+app.get('/repository/:id', async ({ params, query }, res) => {
+  const eager = !!query.eager
+  const public = !!query.public
+  const repository = await garment.source().get(params.id, { eager })
+  if (public) await repository.makePublic()
+  res.json(repository)
+})
+
+app.listen(config.port, () => {
+  log(`Garment API example listening on port ${config.port}`)
 })
