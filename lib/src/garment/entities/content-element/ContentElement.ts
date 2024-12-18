66a84e3e19a764d27645284f1a46ed6af3a4cb58
underscope <underscope@gmail.com>
2024-12-18 10:48:54 +0100

    Replace camelcase with to-camel-case

--- lib/src/garment/entities/content-element/ContentElement.ts
+++ lib/src/garment/entities/content-element/ContentElement.ts
@@ -1,9 +1,9 @@
 import bytes from 'bytes'
-import camelCase from 'camelcase'
 import { Transform, Type } from 'class-transformer'
 import sizeof from 'object-sizeof'
 
 import set from 'set-value'
+import camelCase from 'to-camel-case'
 import { type GraphNodeArray, NodeType } from '../../content-graph'
 
 // seconds, 12 hrs
